5f8a8573e474d5e77dcd5888e8610565ccf899c9
Joe Kapusta <joeknome@gmail.com>
2016-03-22 11:59:57 -0400

    Connected to Redis and added middleware

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var session = require('express-session');
+var RedisStore = require('connect-redis')(session);
+var url = require('url');
 //} requires
 
 // connect to database
@@ -17,6 +19,18 @@ var db = mongoose.connect(dbURL, function(err) {
 		throw err;
 	}
 });
+
+var redisURL = {
+	hostname: 'localhost',
+	port: 6379
+};
+
+var redisPASS;
+
+if(process.env.REDISCLOUD_URL) {
+	redisURL = url.parse(process.env.REDISCLOUD_URL);
+	redisPASS = redisURL.auth.split(":")[1];
+}
 
 // pull in routes
 var router = require('./router.js');
@@ -31,6 +45,11 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(session({
 	key: "sessionid",
+	store: new RedisStore({
+		host: redisURL.hostname,
+		port: redisURL.port,
+		pass: redisPASS
+	}),
 	secret: 'Domo Arigato',
 	resave: true,
 	saveUninitialized: true
